7ef209bfd86ac8016025303d7846e7caa76c68b1
wormholecowboy <wormholecowboy@gmail.com>
2022-04-22 20:21:57 -0400

    first commit

---
+++ hh/contracts/CryptoDevs.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+
+    string _baseTokenURI;
+    uint256 public _price = 0.01 ether;
+    bool public _paused;
+    uint256 public maxTokenIds = 20;
+    uint256 public tokenIds;
+    IWhitelist whitelist;
+    bool public presaleStarted;
+    uint256 public presaleEnded;
+    modifier onlyWhenNotPaused {
+        require(!_paused, "Contract currently paused");
+        _;
+    }
+
+    constructor (string memory baseURI, address whitelistContract) ERC721("Crypto Devs", "CD") {
+        _baseTokenURI = baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+    function presaleMint() public payable onlyWhenNotPaused {
+        require(presaleStarted & block.timestamp < presaleEnded);
+        require(whitelist.whitelistedAddresses(msg.sender), "You are not whitelisted");
+        require(tokenIds < maxTockenIds, "Exceeded maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    function mint() public payable onlyWhenNotPaused {
+        require(presaleStarted && block.timestamp >= presaleEnded, "Presale has not ended yet");
+        require(tokenIds < maxTokenIds, "Exceed maximum Crypto Devs supply");
+        require(msg.value >= _price, "Ether sent is not correct");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
+
+
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setPaused(bool val) public onlyOwner {
+        _paused = val;
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send Ether);
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
+
+    // import ERC721enum, Ownable, Whitelist interface
+    /*
+    create all variable
+    tokenURI, paused, max, tokenCount, whitelist, presale start & end(uint), modifier for OnlyWhenNotPaused\
+
+    constructor
+    takes baseURI, whitelist address (ERC takes name and ticker)
+
+    start presale
+    set to true
+    add minutes to end timestamp
+
+    presale mint
+    require presale started && not ended, in whitelist, < max tokens, eth sent is >+ price
+    increment tokens count
+    safemint?
+
+    mint
+    same as above
+
+    override ERC721 baseURI to return correct one, not empty string
+
+    set paused state of contract, onlyOwner
+
+    withraw funds
+    var owner
+    var amount to take
+    ????
+
+
+    set contract to allow payements in both ways (msg.data)
+    */
+
+    
+    
+}
